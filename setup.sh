dbeafa84b107cacbc75cbed9245411297dc4070d
pprabh2007 <pprabh2007@hotmail.co.uk>
2019-02-10 03:00:17 +0530

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-pip install flask
-pip install numpy
-pip install matplotlib
-pip install pillow
+pip3 install --user flask
+pip3 install --user numpy
+pip3 install --user matplotlib
+pip3 install --user pillow
 
 export FLASK_APP=app.py
 
